160e3584be073cbbdc74867740f5f7124a7741fe
Vladyslav <vladyslavkap@ext.inditex.com>
2023-09-21 21:33:04 +0200

    first commit

---
+++ PROG01_miPrimerProyecto/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Módulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01");
+        System.out.println("Introducción a la programación");
+        System.out.println("Vladyslav Kapkan Panchenko");
+        System.out.println("Cartagena Murcia");
+        System.out.println("21/09/2023");
+        System.out.println("Programa1");
+    }
+}
