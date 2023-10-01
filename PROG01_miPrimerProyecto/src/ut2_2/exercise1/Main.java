b44c8551dd4033b6dcdc480855dc43c44f42ae3c
Vladyslav <vladyslavkap@ext.inditex.com>
2023-10-01 13:12:26 +0200

    refactor(Main.java): change variable name from valorMaximo to VALOR_MAXIMO for better readability and adherence to naming conventions refactor(Main.java): change data type of saldoCuentaBancaria from double to float to match the precision of the value refactor(Main.java): change data type of distanciaTierraJupiter from double to int to represent the distance in whole numbers refactor(Main.java): add units to the printed values of milisegundosDesde1970, saldoCuentaBancaria, and distanciaTierraJupiter for clarity

--- PROG01_miPrimerProyecto/src/ut2_2/exercise1/Main.java
+++ PROG01_miPrimerProyecto/src/ut2_2/exercise1/Main.java
@@ -3,17 +3,17 @@ package ut2_2.exercise1;
 public class Main {
 
   public static void main(String[] args) {
-    final int valorMaximo = 5000;
+    final int VALOR_MAXIMO = 5000;
     boolean tieneCarnetConducir = true;
     byte mesNumero = 9;
     String mesCadena = "Septiembre";
     String nombreCompleto = "Juan Pérez";
     SEXO sexo = SEXO.M;
     long milisegundosDesde1970 = System.currentTimeMillis();
-    double saldoCuentaBancaria = 1000.50;
-    double distanciaTierraJupiter = 628.743;
+    float saldoCuentaBancaria = 1000.50f;
+    int distanciaTierraJupiter = 590000000;
 
-    System.out.println("Valor máximo no modificable: " + valorMaximo);
+    System.out.println("Valor máximo no modificable: " + VALOR_MAXIMO);
     System.out.println(
       "Tiene carnet de conducir: " + (tieneCarnetConducir ? "Sí" : "No")
     );
@@ -22,10 +22,10 @@ public class Main {
     System.out.println("Nombre y apellidos: " + nombreCompleto);
     System.out.println("Sexo: " + (sexo == SEXO.M ? "Masculino" : "Femenino"));
     System.out.println(
-      "Milisegundos desde 01/01/1970: " + milisegundosDesde1970
+      "Milisegundos desde 01/01/1970: " + milisegundosDesde1970 + " ms"
     );
-    System.out.println("Saldo de la cuenta bancaria: " + saldoCuentaBancaria);
-    System.out.println("Distancia a Júpiter (kms): " + distanciaTierraJupiter);
+    System.out.println("Saldo de la cuenta bancaria: " + saldoCuentaBancaria + " €");
+    System.out.println("Distancia a Júpiter (kms): " + distanciaTierraJupiter + " kms");
   }
 
   public enum SEXO {
